3f6337f9f0711b5d0fa275a69b5d7eeda932dca3
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-04-11 01:19:21 +0300

    Add store.js

--- src/components/Navlink.js
+++ src/components/Navlink.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './navlink.css';
+import logo from '../photo/logo.png';
 
 const Navlink = () => (
   <>
     <nav className="navbar">
       <div className="logo">
-        <image />
+        <img src={logo} alt="logo" />
+        <h3>Space Travelers&apos; Hub</h3>
       </div>
       <div className="nav">
         <NavLink to="rockets">Rockets</NavLink>
