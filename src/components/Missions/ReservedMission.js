d22ef0f1314c35a9ca6be3d01067f64148aa5e18
Kevin Okoth <kevinokotth04@gmail.com>
2023-04-12 11:15:50 +0300

    Update reserved mission code

--- src/components/Missions/ReservedMission.js
+++ src/components/Missions/ReservedMission.js
@@ -1,27 +1,21 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
 
 const ReservedMission = () => {
   const reservedMissions = useSelector(
-    (state) => state.mission.reservedMissions,
+    (state) => state.mission.reservedMissions
   );
 
   return (
     <div>
       <h3>My Missions</h3>
-      <ul>
-        {reservedMissions.length === 0 ? (
-          <p>
-            No missions joined!
-            <Link to="/missions">Join mission</Link>
-          </p>
-        ) : (
-          reservedMissions.map((mission) => (
+      {reservedMissions.length > 0 && (
+        <ul>
+          {reservedMissions.map((mission) => (
             <li key={mission.id}>{mission.name}</li>
-          ))
-        )}
-      </ul>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
